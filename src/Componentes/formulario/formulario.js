d0b7f43d1291e2489d29f28729f2a01b2d50e586
Renato <re.fbarcellos@hotmail.com>
2020-05-03 19:18:41 -0400

    Teste envio de email netlify v2

--- src/Componentes/formulario/formulario.js
+++ src/Componentes/formulario/formulario.js
@@ -16,10 +16,10 @@ function Formulario(){
                     <h2 class='titulo'>Contato</h2>
 
                     <form action='POST'name="contact" method="POST" data-netlify="true" class='form'>
-                        <input type="text" name="name"  placeholder="Nome" class='field'/>               
-                        <input type="email" name="email" placeholder='E-mail' class='field'/>              
-                        <textarea  name="message" placeholder='Digite sua mensagem aqui...' class='text'/>              
-                        <button name='botao' type="submit" class='botao'>Enviar</button>           
+                        <input type="text" name="name" id='name' placeholder="Nome" class='field'/>               
+                        <input type="email" name="email"  id='email'placeholder='E-mail' class='field'/>              
+                        <textarea  name="message" id='message'placeholder='Digite sua mensagem aqui...' class='text'/>              
+                        <button type="submit" name='botao'  class='botao'>Enviar</button>           
                     </form>
                     
                 </div>            
