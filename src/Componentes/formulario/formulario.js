38f9bd51144b77f696720ae87741f76e36197d83
Renato <re.fbarcellos@hotmail.com>
2020-05-03 18:26:44 -0400

    Teste envio de email via netlifly

--- src/Componentes/formulario/formulario.js
+++ src/Componentes/formulario/formulario.js
@@ -3,11 +3,7 @@ import React from 'react'
 //importação da estilização da pagina
 import './style.css'
 
-/*importação do arquivo php para envio automatico de email com o 
-preenchimento do formulário
 
-obs: não concluido, falta ligar com o sevidor*/
-import php from '../../email.php'
 
 
 //função que retorna o formulário
@@ -17,13 +13,15 @@ function Formulario(){
         <hr/>
             <section class='contact'>
                 <div class='contato'>
-                    <h2 style={{padding:"40px", paddingBottom:'10px', paddingLeft:'10px'}}>Contato</h2>
-                    <form class='form' method='POST' action='../../email.php'>
-                        <input type="text" name='name' placeholder='Nome' class='field'/>            
-                        <input type="email" placeholder='E-mail' name='email'  class='field'/>            
-                        <textarea name="message" placeholder='Digite sua mensagem aqui'  class='text'/>
-                        <input type='submit' class='botao'/>            
+                    <h2 class='titulo'>Contato</h2>
+
+                    <form action='POST'name="contact" method="POST" data-netlify="true" class='form'>
+                        <input type="text" name="name"  placeholder="Nome" class='field'/>               
+                        <input type="email" name="email" placeholder='E-mail' class='field'/>              
+                        <textarea  name="message" placeholder='Digite sua mensagem aqui...' class='text'/>              
+                        <button name='botao' type="submit" class='botao'>Enviar</button>           
                     </form>
+                    
                 </div>            
             </section>
         </div>   
