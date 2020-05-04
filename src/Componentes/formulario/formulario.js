6a361ca0fc83c37ddec596c08d79c9212093c837
Renato <re.fbarcellos@hotmail.com>
2020-05-03 20:14:43 -0400

    teste envio de email netlify

--- src/Componentes/formulario/formulario.js
+++ src/Componentes/formulario/formulario.js
@@ -15,14 +15,37 @@ function Formulario(){
                 <div class='contato'>
                     <h2 class='titulo'>Contato</h2>
 
-                    <form name="contact" method="POST" data-netlify="true" class='form'>
+                    <form name="contact" action='POST'method="POST" data-netlify="true" class='form'>
                         <input type="text" name="name" id='name' placeholder="Nome" class='field'/>               
                         <input type="email" name="email"  id='email'placeholder='E-mail' class='field'/>              
                         <textarea  name="message" id='message'placeholder='Digite sua mensagem aqui...' class='text'/>              
                         <button type="submit" name='botao'  class='botao'>Enviar</button>           
                     </form>
                     
-                </div>            
+                </div>      
+                <div>
+                <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
+                </div>
             </section>
         </div>   
     )
