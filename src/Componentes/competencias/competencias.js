2b7a52891e70b1726355c23b781d76a1a7ea7695
Renato <re.fbarcellos@hotmail.com>
2020-05-18 14:00:53 -0400

    animação+ transformação em simbulos das linguagens

--- src/Componentes/competencias/competencias.js
+++ src/Componentes/competencias/competencias.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import comp from '../../assents/comp.png'
-
-
+import './style.css'
+//importação dos icons
+import {FaPython, FaReact,FaCuttlefish } from 'react-icons/fa'
+import {DiDatabase } from 'react-icons/di'
 //função que retorna a imagem das competencias
 function Competencias() {
 
   return (
   
-    <div>
-        <img  className='comp' src={comp} alt='abilidades'></img>
+    <div style={{display:"inline" }} >
+      <FaPython className='one_comp' size='150px'/>
+      <FaReact className='one_comp' size='150px'/>
+      <DiDatabase className='bd' size='180px' />
+        
+      <img  className='comp' src={comp}/>
     </div>
   );
 }
