4cf118c9cea96e18f2c2420328a15d6b4418feed
Renato <re.fbarcellos@hotmail.com>
2020-06-06 18:16:09 -0400

    erro linkedin arrumado. aumento abstração

--- src/Pages/projetos/index.js
+++ src/Pages/projetos/index.js
@@ -7,67 +7,61 @@ import './style.css';
 import Header from '../../Componentes/header/header'
 import Footer from '../../Componentes/footer/footer'
 import Contato from '../../Componentes/contato/contato'
+import Projeto_desc from './projeto_desc'
 
-
+//importação das fotos dos projetos 
 import Renato from '../../assents/renato.png'
 import hero from '../../assents/port/bethehero.png'
-import {FaGithubSquare } from 'react-icons/all'
+import loopyng from '../../assents/port/loopyng.png'
+
 //função que retorna a pagina HTML
 function Projetos() {
-    let redirect_rep = () => {
-        window.open('https://github.com/RenatoCesarF/About_me')
-    }
-    let redirect_rep_hero = () => {
-        window.open('https://github.com/RenatoCesarF/be-the-hero')
-    }
+
+
     return (
         <div>
             <Header />
             <div className='back'>
                 <section className='center'>
+
+                    <Projeto_desc
+                        titulo='Loopyng'
+                        texto=' Um programa desenvolvido em python com objetivo de replicar uma loop-station.
+                                O programa funciona atravez das teclas do computador, cada uma das que são mostradas 
+                                no leyout representa um pedal de loop-station, ou seja, pode-se gravar em um audio em 
+                                cada um deles e coloca-lo em looping. Existem funções ainda em desenvolvimento, como:
+                                pause e play do audio metronomo, visualização do grafico do microfone e assim por diante.
+                                Veja mais sobre o projeto no Read Me do github.'
+                        image={loopyng}
+                        link_to='https://github.com/RenatoCesarF/Loopyng'
+                        tamanho = '30%'
+                   />
+                    <Projeto_desc
+                        titulo='Este Blog'
+                        texto=' Site desenvolvido com React-js para testar minhas abilidades em CSS e com a
+                                ferramenta ja mencionada, além de servir como portfólio Online de divulgação do meu 
+                                trabalho. Inspirado pelo Lucas Montano que diz ser importante ter um site "About me" 
+                                para divulgar seus projetos e trabalhos. Não foi possível, até então, implementar todas 
+                                as ideias que tive para o projeto, mas assim que eu aprender mais sobre o framework 
+                                voltarei a desenvolver este site. Veja mais sobre no repositório do github.'
+                        image={Renato}
+                        link_to = 'https://github.com/RenatoCesarF/About_me'
+                    />  
+
+                    <Projeto_desc
+                        titulo='Be The Hero'
+                        texto= "Aplicação feita durante a Semana OmniStak da RockesSeat.
+                                Tem como objetivo aproximar ONGs a possíveis doadores.
+                                A aplicação funciona com um cadastro, atravez do Site, 
+                                por parte da ONG de um dos casos que a mesma necessita de doação. 
+                                A pessoa que tiver o aplicativo pode visualizar esses casos
+                                cadastrados e contactar a ONG caso deseje, esse contato pode
+                                se feito atravez de e-mail ou WhatsApp. Veja mais no repositório do github "
+                        image={hero}
+                        link_to= 'https://github.com/RenatoCesarF/be-the-hero'
+                    />
                    
-                    <section>
-                        <h2 style={{padding:"1cm"}}>Este Blog</h2>
-                        <p className='text_project'> Site desenvolvido com React-js para testar minhas abilidades em CSS e com a
-                            ferramenta ja mencionada, além de servir como portfólio Online de divulgação do meu 
-                            trabalho. Inspirado pelo Lucas Montano que diz ser importante ter um site "About me" 
-                            para divulgar seus projetos e trabalhos. Não foi possível, até então, implementar todas 
-                            as ideias que tive para o projeto, mas assim que eu aprender mais sobre o framework 
-                            voltarei a desenvolver este site.
-                        </p>
-                        <img className='img_renato'src={Renato}alt='imagem projeto'/>
-                            <h4  className='git_hub_link' onClick={redirect_rep}>
-                            <FaGithubSquare
-                                className='link_github'
-                                size='50px'
-                            />
-                            Git Hub
-                            </h4>
-                        <hr/>
-                    </section>
-
-
-                    <section>
-                        <h2 style={{padding:"1cm"}}>Be The Hero</h2>
-                        <p className= 'text_project'>
-                            Aplicação feita durante a Semana OmniStak da RockesSeat.
-                            Tem como objetivo aproximar ONGs a possíveis doadores.
-                             A aplicação funciona com um cadastro, atravez do Site, 
-                             por parte da ONG de um dos casos que a mesma necessita de doação. 
-                             A pessoa que tiver o aplicativo pode visualizar esses casos
-                              cadastrados e contactar a ONG caso deseje, esse contato pode
-                               se feito atravez de e-mail ou WhatsApp.
-                        </p>
-                        <img className='img_renato' src={hero} alt='imagem projeto' />
-                            <h4  className='git_hub_link' onClick={redirect_rep_hero}>
-                            <FaGithubSquare
-                                className='link_github'
-                                size='50px'
-                            />
-                            Git Hub
-                            </h4>
-                        <hr />
-                    </section>
+                   
 
                     <Contato/> 
                 </section>
