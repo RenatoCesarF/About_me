34be20320d3a753d9348a1401ced6058713b13dc
RenatoCesarF <re.fbarcellos@hotmail.com>
2022-08-08 06:40:14 -0400

    Trying to fix deploy bug

--- src/Pages/MyPosts/post.js
+++ src/Pages/MyPosts/post.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import {SiTwitch} from 'react-icons/si'
-import {FaYoutube, FaGithubSquare} from 'react-icons/fa'
-import copyToClipboard from '../../utils/copyToClipboard'
+import { SiTwitch } from 'react-icons/si'
+import { FaYoutube, FaGithubSquare } from 'react-icons/fa'
+
 const LinkName = {
-	YOUTUBE: "Youtube",
-	TWITCH: "Twitch",
-	GITHUB: "GitHub",
+    YOUTUBE: "Youtube",
+    TWITCH: "Twitch",
+    GITHUB: "GitHub",
     DEFAULT: "",
 }
 
-function getLinkNameByURL(url){
-    if(url == null) return LinkName.DEFAULT
-    if(url.includes("youtube") || url.includes("youtu.b")){
+function getLinkNameByURL(url) {
+    if (url == null) return LinkName.DEFAULT
+    if (url.includes("youtube") || url.includes("youtu.b")) {
         return LinkName.YOUTUBE;
     }
-    if(url.includes("twitch")){
+    if (url.includes("twitch")) {
         return LinkName.TWITCH;
     }
-    
-    if(url.includes("github")){
+
+    if (url.includes("github")) {
         return LinkName.GITHUB;
     }
     return LinkName.DEFAULT
@@ -26,36 +26,36 @@ function getLinkNameByURL(url){
 
 function Post(props) {
     const linkName = getLinkNameByURL(props.link);
- 
+
     return (
         <div>
             <h1 className='titulo_post'>{props.tittle}</h1>
             <div>
                 <p className='texto_do_post'>{props.body}</p>
             </div>
-            
+
             <div>
-             
-                <p className="post_link"  onClick={() =>  window.open(props.link)}>
-                    
+
+                <p className="post_link" onClick={() => window.open(props.link)}>
+
                     {
-                        linkName === LinkName.TWITCH?
-                            (<SiTwitch size={30} color="#613FA0"/>)
-                        :
-                        linkName === LinkName.YOUTUBE?
-                            (<FaYoutube size={30}  color='#F20F22'/>)
-                        :
-                        linkName === LinkName.GITHUB?
-                            (<FaGithubSquare size={40}  color='#2d2d2d'/>)
-                        :
-                            null
-                        }
-                    
+                        linkName === LinkName.TWITCH ?
+                            (<SiTwitch size={30} color="#613FA0" />)
+                            :
+                            linkName === LinkName.YOUTUBE ?
+                                (<FaYoutube size={30} color='#F20F22' />)
+                                :
+                                linkName === LinkName.GITHUB ?
+                                    (<FaGithubSquare size={40} color='#2d2d2d' />)
+                                    :
+                                    null
+                    }
+
                 </p>
-              
+
             </div>
             <footer className="post_date">{props.date} </footer>
-            <hr className='linha'/>
+            <hr className='linha' />
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 //Importing React Libs
 import React from 'react'
-import { Routes, Route, BrowserRouter, } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 //Importing all the pages
+import Contributors from './pages/Contributors/index';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import MyPosts from './pages/MyPosts';
 import Footer from './Components/footer/footer';
 import Header from './Components/header/header';
-import Contributors from './pages/Contributors/index';
 
 // Add all the pages into a switch router system
 //"Switch"  is used to switch between the pages 
